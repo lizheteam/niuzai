e2748757a2a813769a394cdac4867e1b2735160a
lizhedegithub <lilizhe150369@163.com>
2018-12-06 22:36:06 +0800

    12/6 --2

---
+++ Assets/Scripts/net/FightHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using ClientNetFrame;
+using System;
+
+public class FightHandler : MonoBehaviour, IHandler
+{
+    public void MessageReceive(SocketModel model)
+    {
+
+    }
+}
